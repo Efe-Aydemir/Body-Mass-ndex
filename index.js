d78a0b933ca4f3e1c9a9926a6e67ab832379b694
Efe Aydemir <delidoluefe42@gmail.com>
2022-05-20 14:29:08 +0300

    Update

--- index.js
+++ index.js
@@ -14,7 +14,13 @@ const card = `
 <h1>Result</h1>
 <h1>Body Mass İndex<br><span id='sha_res'>${result_2 ? result_2.toFixed() : 'Please Enter Number'}</span>
 </h1>
+<br>
+
+<div id='btns'>
+<button id='back_btn' onclick='window.location.reload()'>Go Back</button>
+</div>
 <hr>
+<div id='result'>
 <p>0 to 18.4 BMI: Underweight. In case of encountering this value, which indicates that the person's weight is insufficient for their height, it is recommended that the person gain weight in a healthy way, accompanied by a dietitian.</p>
 <hr>
 <p>18.5 to 24.9 BMI: Normal. This range of values indicates that the person is at the ideal weight. It is recommended that people with this value continue to eat a regular, balanced and healthy diet.</p>
@@ -33,8 +39,6 @@ Age 35-44: BMI of 21-26.<br>
 Age 45-54: BMI of 22-27.<br>
 Age 55-64: BMI of 23-28.<br>
 Age 65 and over: BMI of 24-29.
-<div id='btns'>
-<button id='back_btn' onclick='window.location.reload()'>Go Back</button>
 </div>
 </div>
 `
